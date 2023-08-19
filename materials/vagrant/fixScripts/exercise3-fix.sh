34d49beb2e0d27267d23636547e74b8f89dff2f1
OhadOliver <86773690+OhadOliver@users.noreply.github.com>
2023-08-19 16:22:00 +0300

    Update exercise3-fix.sh

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise3 here
+sudo service apache2 start
+sudo sed -i 's/denied/granted/g' /etc/apache2/sites-available/000-default.conf
+#sudo cat /etc/apache2/sites-available/000-default.conf
+sudo service apache2 restart
