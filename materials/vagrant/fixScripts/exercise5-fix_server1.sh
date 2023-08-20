51a6a3c5bdc99b88034cc8c65ba08a1b1e8854ad
OhadOliver <86773690+OhadOliver@users.noreply.github.com>
2023-08-20 19:35:04 +0300

    Update exercise5-fix_server1.sh

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
 ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+sudo apt update
 sudo apt install sshpass -y
 sshpass -p vagrant ssh-copy-id vagrant@server2
