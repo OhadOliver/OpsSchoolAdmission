fc1538ac93f55294796fa7d6ad810b0388d0e4cc
OhadOliver <86773690+OhadOliver@users.noreply.github.com>
2023-08-24 08:51:40 +0300

    Update exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+script6(){
+	if [ $HOSTNAME == "server1" ]; then
+		scp $1 $2 vagrant@server2:$3
+
+	elif [ $HOSTNAME == "server2" ]; then
+		scp $1 $2 vagrant@server1:$3
+
+	fi
+}
+script6
+sum = $(( $(wc -c < $1) + $(wc -c < $2)))
+echo "$sum"
